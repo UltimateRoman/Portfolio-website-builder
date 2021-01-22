622b5951eb8daee9cdbf1810d914972c7c6bd7d2
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-01-22 21:46:56 +0530

    Some improvements

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from bs4 import BeautifulSoup
+
 def main():
+    with open("portfolio/index.html") as f1:
+        inst = BeautifulSoup(f1, 'html.parser')
+        tag1 = inst.title
+        
+        f1.close()
+        
+    with open("portfolio/index.html", "w") as f2:
+        f2.write(str(inst))
+        print("Your portfolio website has been generated in the /portfolio directory.")
 
 if __name__ == '__main__':
     main()
